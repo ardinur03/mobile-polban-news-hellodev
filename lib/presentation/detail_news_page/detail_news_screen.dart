c023b48a8209c27a9eba080392c82ae711ef0fbf
DESKTOP-9O4EN1D\Irfan <irfankhoerul12@gmail.com>
2023-04-17 11:37:27 +0700

    FIX : Add Button Exit, Pop up Image, and Update Top margin

--- lib/presentation/detail_news_page/detail_news_screen.dart
+++ lib/presentation/detail_news_page/detail_news_screen.dart
@@ -21,34 +21,84 @@ class DetailNewsScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: 20),
-                  Container(
-                    width: double.infinity,
-                    height: 270,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 7,
-                          offset: Offset(0, 3),
+                  SizedBox(height: 38),
+                  GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (_) => Dialog(
+                          child: Container(
+                            width: double.infinity,
+                            height: 400,
+                            child: ClipRRect(
+                              child: InteractiveViewer(
+                                child: Image.network(
+                                  news.galleries.isNotEmpty
+                                      ? news.galleries[0]
+                                      : 'https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 270,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 7,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image(
+                              image: NetworkImage(
+                                news.galleries.isNotEmpty
+                                    ? news.galleries[0]
+                                    : 'https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg',
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 10,
+                          left: 10,
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.pop(
+                                  context); // Replace with your navigation code
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(57, 224, 224, 224),
+                              ),
+                              child: Icon(
+                                Icons.arrow_back,
+                                color: Colors.black,
+                                size: 30,
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image(
-                        image: NetworkImage(
-                          news.galleries.isNotEmpty
-                              ? news.galleries[0]
-                              : 'https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg',
-                        ),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
                   ),
-                  SizedBox(height: 10),
+                  SizedBox(height: 15),
                   Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
@@ -59,7 +109,7 @@ class DetailNewsScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 10),
+                  SizedBox(height: 15),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
